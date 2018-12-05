a29b7f463fabf5c0f1c1d9413ce85e7b4eb6e53b
wenhao176 <921790476@qq.com>
2018-12-05 14:39:48 +0800

    添加注解类开启eureka服务,添加spring security 认证包

--- src/main/java/com/wenhao/microservice/microservicediscovereurekaauth/MicroserviceDiscoverEurekaAuthApplication.java
+++ src/main/java/com/wenhao/microservice/microservicediscovereurekaauth/MicroserviceDiscoverEurekaAuthApplication.java
@@ -2,8 +2,10 @@ package com.wenhao.microservice.microservicediscovereurekaauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MicroserviceDiscoverEurekaAuthApplication {
 
     public static void main(String[] args) {

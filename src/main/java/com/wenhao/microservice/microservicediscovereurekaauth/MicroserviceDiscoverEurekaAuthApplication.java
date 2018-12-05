b5345ad8ee554aca8d3735f804380ca149eb3a67
wenhao176 <921790476@qq.com>
2018-12-05 14:23:57 +0800

    Initial commit

---
+++ src/main/java/com/wenhao/microservice/microservicediscovereurekaauth/MicroserviceDiscoverEurekaAuthApplication.java
@@ -0,0 +1,12 @@
+package com.wenhao.microservice.microservicediscovereurekaauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceDiscoverEurekaAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceDiscoverEurekaAuthApplication.class, args);
+    }
+}
---
+++ src/test/java/com/wenhao/microservice/microservicediscovereurekaauth/MicroserviceDiscoverEurekaAuthApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wenhao.microservice.microservicediscovereurekaauth;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroserviceDiscoverEurekaAuthApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
